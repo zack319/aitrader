1ca1a04be57bbf4625323ce0bc8ef97838c09539
Zak Jaouad <zakaria@everybill.com>
2020-03-07 17:20:44 -0500

    AITrader: Added build and modified server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,12 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../build')));
 
 const uri = process.env.MONGODB_URI;
 // mongoose.connect(uri, {
---
+++ build/precache-manifest.2bf1a58e2a694a17e621b875fbc927ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef917783de102e628126e82cb7e52629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42763c2f92eb3f7f527a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d65068c5784f41949f25",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42763c2f92eb3f7f527a",
+    "url": "/static/js/2.273a484d.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.273a484d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65068c5784f41949f25",
+    "url": "/static/js/main.a431f087.chunk.js"
+  },
+  {
+    "revision": "c376af3f6ad51a46ce23",
+    "url": "/static/js/runtime-main.3c02beb1.js"
+  }
+]);
